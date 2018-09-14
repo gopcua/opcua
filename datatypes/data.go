ecd001c8f1909733f048fd091d10bcfddf05b940
Mirco Zeiss <mirco.zeiss@gmail.com>
2018-09-14 16:20:11 +0200

    add copyright to data and variant

--- datatypes/data.go
+++ datatypes/data.go
@@ -1,3 +1,7 @@
+// Copyright 2018 gopcua authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
 package datatypes
 
 // Data is an interface to handle any kind of OPC UA data types.
