6669eff7933c9ca25293a22d75c706e3de52155a
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2018-09-14 15:31:46 +0900

    improve stability of UASC SecureChannel

--- uasc/doc.go
+++ uasc/doc.go
@@ -3,6 +3,12 @@
 // found in the LICENSE file.
 
 /*
-Package uasc provides encoding/decoding feature for OPC UA Secure Conversation.
+Package uasc provides encoding/decoding and automated secure channel and session handling for OPC UA Secure Conversation.
+
+To establish Secure Channel as a client, use OpenSecureChannel().
+
+To establish Secure Channel as a server, use ListenAndAccept().
+
+Both returns *SecureChannel, which implements net.Conn interface,
 */
 package uasc
---
+++ uasc/secure-channel_test.go
@@ -0,0 +1,174 @@
+// Copyright 2018 gopcua authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package uasc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/wmnsk/gopcua/services"
+	"github.com/wmnsk/gopcua/uacp"
+)
+
+func TestSecureChannel(t *testing.T) {
+	ep := "opc.tcp://127.0.0.1:4840/foo/bar"
+	policyURI := "http://opcfoundation.org/UA/SecurityPolicy#None"
+	ln, err := uacp.Listen(ep, 0xffff)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ln.Close()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	cfg := NewConfig(
+		1, policyURI, nil, nil, 0, 1,
+	)
+
+	go func() {
+		defer ln.Close()
+		srvConn, err := ln.Accept(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := ListenAndAcceptSecureChannel(ctx, srvConn, cfg); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	cliConn, err := uacp.Dial(ctx, ep)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := OpenSecureChannel(ctx, cliConn, cfg, services.SecModeNone, 0xffff, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestClientWrite(t *testing.T) {
+	ep := "opc.tcp://127.0.0.1:4840/foo/bar"
+	policyURI := "http://opcfoundation.org/UA/SecurityPolicy#None"
+	ln, err := uacp.Listen(ep, 0xffff)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ln.Close()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var (
+		cliConn, srvConn *uacp.Conn
+		cliChan, srvChan *SecureChannel
+	)
+
+	cfg := NewConfig(
+		1, policyURI, nil, nil, 0, 1,
+	)
+	go func() {
+		defer ln.Close()
+		srvConn, err = ln.Accept(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		srvChan, err = ListenAndAcceptSecureChannel(ctx, srvConn, cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	cliConn, err = uacp.Dial(ctx, ep)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cliChan, err = OpenSecureChannel(ctx, cliConn, cfg, services.SecModeNone, 0xffff, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := []byte{0xde, 0xad, 0xbe, 0xef}
+	if _, err := cliChan.Write(msg); err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 1024)
+	n, err := srvChan.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(buf[:n], msg); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestServerWrite(t *testing.T) {
+	ep := "opc.tcp://127.0.0.1:4840/foo/bar"
+	policyURI := "http://opcfoundation.org/UA/SecurityPolicy#None"
+	ln, err := uacp.Listen(ep, 0xffff)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ln.Close()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var (
+		cliConn, srvConn *uacp.Conn
+		cliChan, srvChan *SecureChannel
+	)
+
+	cfg := NewConfig(
+		1, policyURI, nil, nil, 0, 1,
+	)
+	go func() {
+		defer ln.Close()
+		srvConn, err = ln.Accept(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		srvChan, err = ListenAndAcceptSecureChannel(ctx, srvConn, cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	cliConn, err = uacp.Dial(ctx, ep)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cliChan, err = OpenSecureChannel(ctx, cliConn, cfg, services.SecModeNone, 0xffff, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := []byte{0xde, 0xad, 0xbe, 0xef}
+	if _, err := srvChan.Write(msg); err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 1024)
+	n, err := cliChan.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(buf[:n], msg); diff != "" {
+		t.Error(diff)
+	}
+}
