e7e5e6f27a400c8fcd070dbfd9ceb4e5d438ef77
Mirco Zeiss <mirco.zeiss@gmail.com>
2018-09-17 15:35:56 +0200

    add constructor, another test and fix encodingmask

--- datatypes/data-value.go
+++ datatypes/data-value.go
@@ -24,6 +24,44 @@ type DataValue struct {
 	ServerPicoSeconds uint16
 }
 
+// NewDataValue creates a new DataValue.
+func NewDataValue(hasValue, hasSrcTs, hasSrcPs, hasSvrTs, hasServerPc bool, v *Variant, status uint32, srcTs time.Time, srcPs uint16, svrTs time.Time, svrPs uint16) *DataValue {
+	d := &DataValue{
+		Value:             v,
+		Status:            status,
+		SourceTimestamp:   srcTs,
+		SourcePicoSeconds: srcPs,
+		ServerTimestamp:   svrTs,
+		ServerPicoSeconds: svrPs,
+	}
+
+	if d.HasValue() {
+		d.SetValueFlag()
+	}
+
+	if d.HasStatus() {
+		d.SetStatusFlag()
+	}
+
+	if d.HasSourceTimestamp() {
+		d.SetSourceTimestampFlag()
+	}
+
+	if d.HasSourcePicoSeconds() {
+		d.SetSourcePicoSecondsFlag()
+	}
+
+	if d.HasServerTimestamp() {
+		d.SetServerTimestampFlag()
+	}
+
+	if d.HasServerPicoSeconds() {
+		d.SetServerPicoSecondsFlag()
+	}
+
+	return d
+}
+
 // DecodeDataValue decodes given bytes into DataValue.
 func DecodeDataValue(b []byte) (*DataValue, error) {
 	d := &DataValue{}
@@ -164,9 +202,19 @@ func (d *DataValue) HasValue() bool {
 	return d.EncodingMask&0x1 == 1
 }
 
+// SetValueFlag sets value flag in EncodingMask in DataValue.
+func (d *DataValue) SetValueFlag() {
+	d.EncodingMask |= 0x1
+}
+
 // HasStatus checks if DataValue has Status or not.
 func (d *DataValue) HasStatus() bool {
 	return (d.EncodingMask>>1)&0x1 == 1
+}
+
+// SetStatusFlag sets status flag in EncodingMask in DataValue.
+func (d *DataValue) SetStatusFlag() {
+	d.EncodingMask |= 0x2
 }
 
 // HasSourceTimestamp checks if DataValue has SourceTimestamp or not.
@@ -174,17 +222,37 @@ func (d *DataValue) HasSourceTimestamp() bool {
 	return (d.EncodingMask>>2)&0x1 == 1
 }
 
-// HasSourcePicoSeconds checks if DataValue has SourcePicoSeconds or not.
-func (d *DataValue) HasSourcePicoSeconds() bool {
-	return (d.EncodingMask>>3)&0x1 == 1
+// SetSourceTimestampFlag sets source timestamp flag in EncodingMask in DataValue.
+func (d *DataValue) SetSourceTimestampFlag() {
+	d.EncodingMask |= 0x4
 }
 
 // HasServerTimestamp checks if DataValue has ServerTimestamp or not.
 func (d *DataValue) HasServerTimestamp() bool {
+	return (d.EncodingMask>>3)&0x1 == 1
+}
+
+// SetServerTimestampFlag sets server timestamp flag in EncodingMask in DataValue.
+func (d *DataValue) SetServerTimestampFlag() {
+	d.EncodingMask |= 0x8
+}
+
+// HasSourcePicoSeconds checks if DataValue has SourcePicoSeconds or not.
+func (d *DataValue) HasSourcePicoSeconds() bool {
 	return (d.EncodingMask>>4)&0x1 == 1
+}
+
+// SetSourcePicoSecondsFlag sets source picoseconds flag in EncodingMask in DataValue.
+func (d *DataValue) SetSourcePicoSecondsFlag() {
+	d.EncodingMask |= 0x10
 }
 
 // HasServerPicoSeconds checks if DataValue has ServerPicoSeconds or not.
 func (d *DataValue) HasServerPicoSeconds() bool {
 	return (d.EncodingMask>>5)&0x1 == 1
+}
+
+// SetServerPicoSecondsFlag sets server picoseconds flag in EncodingMask in DataValue.
+func (d *DataValue) SetServerPicoSecondsFlag() {
+	d.EncodingMask |= 0x20
 }
--- datatypes/data-value_test.go
+++ datatypes/data-value_test.go
@@ -6,6 +6,7 @@ package datatypes
 
 import (
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -24,6 +25,21 @@ var dataValueTests = []struct {
 			Value:        NewVariant(NewFloat(2.50025)),
 		},
 		length: 6,
+	},
+	{
+		description: "value, source timestamp, server timestamp",
+		bytes: []byte{
+			0x0d, 0x0a, 0xc9, 0x02, 0x20, 0x40, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01,
+		},
+		dv: &DataValue{
+			EncodingMask:    0x0d,
+			Value:           NewVariant(NewFloat(2.50017)),
+			SourceTimestamp: time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+			ServerTimestamp: time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+		},
+		length: 22,
 	},
 }
 
