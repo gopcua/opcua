3d60c01310ca330a1544fe2eddc75b9ceb476af8
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2018-10-03 10:32:30 +0900

    add service WriteRequest

---
+++ services/write-request_test.go
@@ -0,0 +1,217 @@
+// Copyright 2018 gopcua authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package services
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/wmnsk/gopcua/datatypes"
+)
+
+var writeRequestCases = []struct {
+	description string
+	structured  *WriteRequest
+	serialized  []byte
+}{
+	{
+		"single-writevalue",
+		NewWriteRequest(
+			NewRequestHeader(
+				datatypes.NewOpaqueNodeID(0x00, []byte{
+					0x08, 0x22, 0x87, 0x62, 0xba, 0x81, 0xe1, 0x11,
+					0xa6, 0x43, 0xf8, 0x77, 0x7b, 0xc6, 0x2f, 0xc8,
+				}),
+				time.Date(2018, time.August, 10, 23, 0, 0, 0, time.UTC),
+				1, 0, 0, "", NewNullAdditionalHeader(), nil,
+			),
+			datatypes.NewWriteValue(
+				datatypes.NewFourByteNodeID(0, 2256),
+				datatypes.IntegerIDValue,
+				"",
+				datatypes.NewDataValue(
+					true, false, true, false, true, false,
+					datatypes.NewVariant(datatypes.NewFloat(2.50017)),
+					0,
+					time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+					0,
+					time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+					0,
+				),
+			),
+		),
+		[]byte{
+			// TypeID
+			0x01, 0x00, 0xa1, 0x02,
+			// RequestHeader
+			// AuthenticationToken
+			0x05, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x08,
+			0x22, 0x87, 0x62, 0xba, 0x81, 0xe1, 0x11, 0xa6,
+			0x43, 0xf8, 0x77, 0x7b, 0xc6, 0x2f, 0xc8,
+			// Timestamp
+			0x00, 0x98, 0x67, 0xdd, 0xfd, 0x30, 0xd4, 0x01,
+			// RequestHandle
+			0x01, 0x00, 0x00, 0x00,
+			// ReturnDiagnostics
+			0x00, 0x00, 0x00, 0x00,
+			// AuditEntryID
+			0xff, 0xff, 0xff, 0xff,
+			// TimeoutHint
+			0x00, 0x00, 0x00, 0x00,
+			// AdditionalHeader
+			0x00, 0x00, 0x00,
+			// NodesToWrite
+			// ArraySize
+			0x01, 0x00, 0x00, 0x00,
+			// NodeID
+			0x01, 0x00, 0xd0, 0x08,
+			// AttributeID
+			0x0d, 0x00, 0x00, 0x00,
+			// IndexRange
+			0xff, 0xff, 0xff, 0xff,
+			// Value
+			0x0d, 0x0a, 0xc9, 0x02, 0x20, 0x40, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01,
+		},
+	},
+	{
+		"multiple-writevalue",
+		NewWriteRequest(
+			NewRequestHeader(
+				datatypes.NewOpaqueNodeID(0x00, []byte{
+					0x08, 0x22, 0x87, 0x62, 0xba, 0x81, 0xe1, 0x11,
+					0xa6, 0x43, 0xf8, 0x77, 0x7b, 0xc6, 0x2f, 0xc8,
+				}),
+				time.Date(2018, time.August, 10, 23, 0, 0, 0, time.UTC),
+				1, 0, 0, "", NewNullAdditionalHeader(), nil,
+			),
+			datatypes.NewWriteValue(
+				datatypes.NewFourByteNodeID(0, 2256),
+				datatypes.IntegerIDValue,
+				"",
+				datatypes.NewDataValue(
+					true, false, true, false, true, false,
+					datatypes.NewVariant(datatypes.NewFloat(2.50017)),
+					0,
+					time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+					0,
+					time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+					0,
+				),
+			),
+			datatypes.NewWriteValue(
+				datatypes.NewFourByteNodeID(0, 2256),
+				datatypes.IntegerIDValue,
+				"",
+				datatypes.NewDataValue(
+					true, false, true, false, true, false,
+					datatypes.NewVariant(datatypes.NewFloat(2.50017)),
+					0,
+					time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+					0,
+					time.Date(2018, time.September, 17, 14, 28, 29, 112000000, time.UTC),
+					0,
+				),
+			),
+		),
+		[]byte{
+			// TypeID
+			0x01, 0x00, 0xa1, 0x02,
+			// RequestHeader
+			// AuthenticationToken
+			0x05, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x08,
+			0x22, 0x87, 0x62, 0xba, 0x81, 0xe1, 0x11, 0xa6,
+			0x43, 0xf8, 0x77, 0x7b, 0xc6, 0x2f, 0xc8,
+			// Timestamp
+			0x00, 0x98, 0x67, 0xdd, 0xfd, 0x30, 0xd4, 0x01,
+			// RequestHandle
+			0x01, 0x00, 0x00, 0x00,
+			// ReturnDiagnostics
+			0x00, 0x00, 0x00, 0x00,
+			// AuditEntryID
+			0xff, 0xff, 0xff, 0xff,
+			// TimeoutHint
+			0x00, 0x00, 0x00, 0x00,
+			// AdditionalHeader
+			0x00, 0x00, 0x00,
+			// NodesToWrite
+			// ArraySize
+			0x02, 0x00, 0x00, 0x00,
+			// NodeID
+			0x01, 0x00, 0xd0, 0x08,
+			// AttributeID
+			0x0d, 0x00, 0x00, 0x00,
+			// IndexRange
+			0xff, 0xff, 0xff, 0xff,
+			// Value
+			0x0d, 0x0a, 0xc9, 0x02, 0x20, 0x40, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01,
+			// NodeID
+			0x01, 0x00, 0xd0, 0x08,
+			// AttributeID
+			0x0d, 0x00, 0x00, 0x00,
+			// IndexRange
+			0xff, 0xff, 0xff, 0xff,
+			// Value
+			0x0d, 0x0a, 0xc9, 0x02, 0x20, 0x40, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01, 0x80, 0x3b,
+			0xe8, 0xb3, 0x92, 0x4e, 0xd4, 0x01,
+		},
+	},
+}
+
+func TestDecodeWriteRequest(t *testing.T) {
+	for _, c := range writeRequestCases {
+		got, err := DecodeWriteRequest(c.serialized)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// need to clear Payload here.
+		got.Payload = nil
+
+		if diff := cmp.Diff(got, c.structured, decodeCmpOpt); diff != "" {
+			t.Errorf("%s failed\n%s", c.description, diff)
+		}
+	}
+}
+
+func TestSerializeWriteRequest(t *testing.T) {
+	for _, c := range writeRequestCases {
+		got, err := c.structured.Serialize()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff(got, c.serialized); diff != "" {
+			t.Errorf("%s failed\n%s", c.description, diff)
+		}
+	}
+}
+
+func TestWriteRequestLen(t *testing.T) {
+	for _, c := range writeRequestCases {
+		got := c.structured.Len()
+
+		if diff := cmp.Diff(got, len(c.serialized)); diff != "" {
+			t.Errorf("%s failed\n%s", c.description, diff)
+		}
+	}
+}
+
+func TestWriteRequestServiceType(t *testing.T) {
+	for _, c := range writeRequestCases {
+		if c.structured.ServiceType() != ServiceTypeWriteRequest {
+			t.Errorf(
+				"ServiceType doesn't match. Want: %d, Got: %d",
+				ServiceTypeWriteRequest,
+				c.structured.ServiceType(),
+			)
+		}
+	}
+}
