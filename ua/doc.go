a90de3823429f85d6a1ceebb3ad48e45fa27183c
Frank Schroeder <frank.schroeder@northvolt.com>
2019-03-17 08:30:04 +0100

    update doc.go

--- ua/doc.go
+++ ua/doc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 
-/*
-Package datatypes defines the structures, decoders and encoder for built-in data types described in Part 6 Section 5 Data encoding.
-*/
+// Package ua defines the structures, decoders and encoder
+// for built-in data types described in Part 6 Section 5 Data encoding
+// and for services in OPC UA Binary Protocol.
 package ua
