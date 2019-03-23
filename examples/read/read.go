da6e7b0914c24c881effc938a9adf18998afda9a
Frank Schroeder <frank.schroeder@northvolt.com>
2019-03-23 17:04:00 +0100

    provide read example

---
+++ examples/read/read.go
@@ -0,0 +1,50 @@
+// Copyright 2018-2019 opcua authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/debug"
+	"github.com/gopcua/opcua/ua"
+)
+
+func main() {
+	endpoint := flag.String("endpoint", "opc.tcp://localhost:4840", "OPC UA Endpoint URL")
+	nodeID := flag.String("node", "", "NodeID to read")
+	flag.BoolVar(&debug.Enable, "debug", false, "enable debug logging")
+	flag.Parse()
+	log.SetFlags(0)
+
+	c := opcua.NewClient(*endpoint, nil)
+	if err := c.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	id, err := ua.NewNodeID(*nodeID)
+	if err != nil {
+		log.Fatalf("invalid node id: %v", err)
+	}
+
+	req := &ua.ReadRequest{
+		MaxAge: 2000,
+		NodesToRead: []*ua.ReadValueID{
+			&ua.ReadValueID{NodeID: id},
+		},
+		TimestampsToReturn: ua.TimestampsToReturnBoth,
+	}
+
+	resp, err := c.Read(req)
+	if err != nil {
+		log.Fatalf("Read failed: %s", err)
+	}
+	if resp.Results[0].Status != ua.StatusOK {
+		log.Fatalf("Status not OK: %v", resp.Results[0].Status)
+	}
+	log.Print(resp.Results[0].Value)
+}
