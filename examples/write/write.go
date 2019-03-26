ce1461d32f77d0b0fc1d6ee6d850dd776f8c377a
Frank Schroeder <frank.schroeder@northvolt.com>
2019-03-26 21:18:46 +0100

    add support for write

---
+++ examples/write/write.go
@@ -0,0 +1,58 @@
+// Copyright 2018-2019 opcua authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/debug"
+	"github.com/gopcua/opcua/ua"
+)
+
+func main() {
+	endpoint := flag.String("endpoint", "opc.tcp://localhost:4840", "OPC UA Endpoint URL")
+	nodeID := flag.String("node", "", "NodeID to read")
+	value := flag.String("value", "", "value")
+	flag.BoolVar(&debug.Enable, "debug", false, "enable debug logging")
+	flag.Parse()
+	log.SetFlags(0)
+
+	c := opcua.NewClient(*endpoint, nil)
+	if err := c.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	id, err := ua.NewNodeID(*nodeID)
+	if err != nil {
+		log.Fatalf("invalid node id: %v", err)
+	}
+
+	v, err := ua.NewVariant(*value)
+	if err != nil {
+		log.Fatalf("invalid value: %v", err)
+	}
+
+	req := &ua.WriteRequest{
+		NodesToWrite: []*ua.WriteValue{
+			&ua.WriteValue{
+				NodeID:      id,
+				AttributeID: ua.IntegerIDValue,
+				Value: &ua.DataValue{
+					EncodingMask: ua.DataValueValue,
+					Value:        v,
+				},
+			},
+		},
+	}
+
+	resp, err := c.Write(req)
+	if err != nil {
+		log.Fatalf("Read failed: %s", err)
+	}
+	log.Printf("%v", resp.Results[0])
+}
