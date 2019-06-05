31e10e8b8d7c2107668a0c1b75f36e8a3bd8eb80
Frank Schroeder <frank.schroeder@northvolt.com>
2019-06-06 00:40:15 +0200

    Update datetime example

--- examples/datetime/datetime.go
+++ examples/datetime/datetime.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/gopcua/opcua"
@@ -15,13 +16,24 @@ import (
 
 func main() {
 	endpoint := flag.String("endpoint", "opc.tcp://localhost:4840", "OPC UA Endpoint URL")
-	policy := flag.String("policy", "None", "Security policy")
-	mode := flag.String("mode", "None", "Security mode")
-	certFile := flag.String("cert", "", "Path to cert.pem")
-	keyFile := flag.String("key", "", "Path to private key.pem")
+	policy := flag.String("policy", "", "Security policy: None, Basic128Rsa15, Basic256, Basic256Sha256. Default: auto")
+	mode := flag.String("mode", "", "Security mode: None, Sign, SignAndEncrypt. Default: auto")
+	certFile := flag.String("cert", "", "Path to cert.pem. Required for security mode/policy != None")
+	keyFile := flag.String("key", "", "Path to private key.pem. Required for security mode/policy != None")
 	flag.BoolVar(&debug.Enable, "debug", false, "enable debug logging")
 	flag.Parse()
 	log.SetFlags(0)
+
+	endpoints, err := opcua.GetEndpoints(*endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ep := opcua.SelectEndpoint(endpoints, *policy, ua.MessageSecurityModeFromString(*mode))
+	if ep == nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("*", ep.SecurityPolicyURI, ep.SecurityMode)
 
 	opts := []opcua.Option{
 		opcua.SecurityPolicy(*policy),
@@ -29,9 +41,10 @@ func main() {
 		opcua.CertificateFile(*certFile),
 		opcua.PrivateKeyFile(*keyFile),
 		opcua.AuthAnonymous(),
+		opcua.SecurityFromEndpoint(ep, ua.UserTokenTypeAnonymous),
 	}
 
-	c := opcua.NewClient(*endpoint, opts...)
+	c := opcua.NewClient(ep.EndpointURL, opts...)
 	if err := c.Connect(); err != nil {
 		log.Fatal(err)
 	}
