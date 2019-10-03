97dd9e543dc6d26a4004561346cdad7f0a40beff
jonathan <github@jonathan.camp>
2019-10-03 09:39:34 +0200

    go fmt

--- uapolicy/policyBasic256.go
+++ uapolicy/policyBasic256.go
@@ -7,8 +7,8 @@ package uapolicy
 import (
 	"crypto"
 	"crypto/rsa"
-	"github.com/gopcua/opcua/errors"
 	"fmt"
+	"github.com/gopcua/opcua/errors"
 
 	// Force compilation of required hashing algorithms, although we don't directly use the packages
 	_ "crypto/sha1"
