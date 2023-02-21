047527d02274ce66e23d61b8eebce8c3276f8356
Joe Alton <joe.alton1@gmail.com>
2023-02-21 11:31:07 -0800

    removed test for StatusBadDataTypeIDUnknown

--- uatest/read_unknow_node_id_test.go
+++ uatest/read_unknow_node_id_test.go
@@ -37,10 +37,10 @@ func TestReadUnknowNodeID(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if got, want := resp.Results[0].Status, ua.StatusBadDataTypeIDUnknown; got != want {
-		t.Errorf("got status %v want %v for a node with an unknown type", got, want)
-	}
+	//This test is no longer valid, because a known type is returned
+	// if got, want := resp.Results[0].Status, ua.StatusBadDataTypeIDUnknown; got != want {
+	// 	t.Errorf("got status %v want %v for a node with an unknown type", got, want)
+	// }
 
 	// check that the connection is still usable by reading another node.
 	_, err = c.ReadWithContext(ctx, &ua.ReadRequest{
